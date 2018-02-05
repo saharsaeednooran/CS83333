408944d742b7b04de0b9ea95746e6b43df7f898e
Sahar Saeednooran <sahar.saeednooran@gmail.com>
2018-02-04 22:06:01 -0500

    Add unit tests

---
+++ Excersise3/E3_saharSAEEDNOORAN/Excrsise3Tests1/Exercise3Tests.cs
@@ -0,0 +1,33 @@
+﻿/* 
+* CST8333-350 Programming Language Research Project
+* Author: Sahar Saeednooran
+* Exercise3Test : sample unit test for Exersise3 using MSTest framework
+*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exersise3;
+
+namespace Exersise3.Tests
+{
+    [TestClass()]
+    public class Exercise3Tests
+    {
+        [TestMethod()]
+        public void CalculateFactorialTest1()
+        {
+            long actual = new Exercise3().CalculateFactorial(5);
+            Assert.AreEqual(120, actual);
+        }
+        [TestMethod()]
+        public void CalculateFactorialTest2()
+        {
+            long actual = new Exercise3().CalculateFactorial(0);
+            Assert.AreEqual(1, actual);
+        }
+        [TestMethod()]
+        public void CalculateFactorialTest3()
+        {
+            long actual = new Exercise3().CalculateFactorial(100);
+            Assert.AreEqual(0, actual);
+        }
+    }
+}
